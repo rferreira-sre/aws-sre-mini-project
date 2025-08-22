58285495a163cde0501104878eec1f974e0ae176
Rafael Ferreira <rferreira.data@gmail.com>
2025-08-21 23:34:21 -0400

    coded provider.tf and variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,34 @@
-# TODO: variables (region, s3_bucket_name, alert_email)
+variable "project_name" {
+  description = "Name prefix for resources"
+  type        = string
+  default     = "aws-sre-mini"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "allow_ssh" {
+  description = "Open SSH (22) to your IP?"
+  type        = bool
+  default     = false
+}
+
+variable "my_ip_cidr" {
+  description = "98.87.98.93/32"
+  type        = string
+  default     = "0.0.0.0/32"
+}
+
+variable "sns_email" {
+  description = "rferreira.data@gmail.com"
+  type        = string
+}
+
+variable "create_bucket" {
+  description = "Create an S3 bucket for artifacts/backups"
+  type        = bool
+  default     = true
+}
